c1a4f1ffb8b671996f4b9797fcaab0d72361b8b9
= <=>
2022-07-13 22:38:42 -0300

    init commit

---
+++ src/main.c
@@ -0,0 +1,90 @@
+// Example program:
+// Using SDL2 to create an application window
+
+#include "SDL2/SDL.h"
+#include <stdio.h>
+
+typedef struct
+{
+  int x;
+  int y;
+  SDL_Texture *texture;
+} Player;
+
+typedef struct
+{
+  SDL_Renderer *renderer;
+  SDL_Window *window;
+} App;
+
+App app;
+Player player;
+
+SDL_Texture *loadTexture(char *filename)
+{
+  SDL_Texture *texture;
+
+  SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename);
+
+  texture = IMG_LoadTexture(app.renderer, filename);
+
+  return texture;
+}
+
+int main(int argc, char *argv[])
+{
+  player.x = 100;
+  player.y = 100;
+  player.texture = loadTexture("./player.png");
+
+  // SDL_Window *window; // Declare a pointer
+
+  SDL_Init(SDL_INIT_VIDEO); // Initialize SDL2
+
+  // Create an application window with the following settings:
+
+  app.window = SDL_CreateWindow(
+      "An SDL2 window",        // window title
+      SDL_WINDOWPOS_UNDEFINED, // initial x position
+      SDL_WINDOWPOS_UNDEFINED, // initial y position
+      640,                     // width, in pixels
+      480,                     // height, in pixels
+      SDL_RENDERER_ACCELERATED // flags - see below
+  );
+
+  // Check that the window was successfully created
+  if (app.window == NULL)
+  {
+    // In the case that the window could not be made...
+    printf("Could not create window: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  app.renderer = SDL_CreateRenderer(app.window, -1, SDL_RENDERER_ACCELERATED);
+
+  // The window is open: could enter program loop here (see SDL_PollEvent())
+  while (app.window)
+  {
+    SDL_SetRenderDrawColor(app.renderer, 250, 195, 235, 255);
+    SDL_RenderClear(app.renderer);
+    SDL_RenderPresent(app.renderer);
+
+    // SDL_Event event;
+
+    // while (SDL_PollEvent(&event))
+    // { // poll until all events are handled!
+    //   // decide what to do with this event.
+    // }
+
+    // update game state, draw the current frame
+  }
+
+  SDL_Delay(3000); // Pause execution for 3000 milliseconds, for example
+
+  // Close and destroy the window
+  SDL_DestroyWindow(app.window);
+
+  // Clean up
+  SDL_Quit();
+  return 0;
+}
